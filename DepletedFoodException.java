d13ef73eba98bc0de0dafd30b8d4683eda0bfc88
Bhaabss <bhaavna.venkatramanarao@students.mq.edu.au>
2026-09-07 13:53:17 +1000

    DepletedFood

--- DepletedFoodException.java
+++ DepletedFoodException.java
@@ -1,3 +1,5 @@
-public class DepletedFoodException {
-    
+public class DepletedFoodException extends Exception {
+    public DepletedFoodException(String message) {
+        super(message);
+    }
 }
